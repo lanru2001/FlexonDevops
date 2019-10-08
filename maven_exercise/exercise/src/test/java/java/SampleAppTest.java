0a34cb680ccaa24c113c20f8b8e0e5e494940906
Aniket Agarwal <agarwaan@tcd.ie>
2019-10-08 16:37:36 -0700

    Finishing Maven Project

--- maven_exercise/exercise/src/test/java/java/SampleAppTest.java
+++ maven_exercise/exercise/src/test/java/java/SampleAppTest.java
@@ -4,10 +4,11 @@ import jdk.jfr.Timestamp;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
+//import static org.junit.jupiter.api.Assertions.*;
+//import org.junit.jupiter.api.Test;
 import jdk.internal.jline.internal.TestAccessible;
-
+import java.lang.*;
+import org.junit.*;
 /**
  * Unit test for SampleApp.
  */
@@ -16,66 +17,43 @@ public class SampleAppTest extends TestCase {
      * Create the test case
      *
      * @param testName name of the test case
-     */
+    */ 
     public SampleAppTest(String testName) {
         super(testName);
     }
 
     /**
      * @return the suite of tests being tested
-     */
+    */
     public static Test suite() {
         return new TestSuite(SampleAppTest.class);
     }
-
-    @Test
-    void getInteger()
-    {
+    
+    /**
+     * Rigourous Test :-)
+    */ 
+    public void testApp() {
         int arr[] = { 1, 2, 4, 5};
         System.out.println("yoyo");
         assertEquals(2,Sample.getInteger(arr, 2));
-    }
-
-    @Test
-    void getMin()
-    {
-        int arr[] = { 1, 2, 4, 5};
+        
         System.out.println("yoyo");
         assertEquals(1,Sample.findMin(arr));
-    }
-
-    @Test
-    void getMax()
-    {
-        int arr[] = { 1, 2, 4, 5};
+        
         System.out.println("yoyo");
-        assertEquals(5,Sample.findMin(arr));
-    }
-
-    @Test
-    void getrev()
-    {
-        int arr[] = { 1, 2, 4, 5};
+        assertEquals(5,Sample.findMax(arr));
+  
         int ror[] = { 5, 4, 2, 1};
         System.out.println("yoyo");
         int[] abv = Sample.reverseArray(arr);
-        assertEquals(ror,abv);
-    }
-    @Test
-    void getsort()
-    {
-        int arr[] = { 1, 2, 4, 5};
-        int ror[] = { 5, 4, 2, 1};
+        Assert.assertArrayEquals( ror, abv );
+        
+        int ar[] = { 1, 2, 3, 5};
+        int ro[] = { 5, 3, 2, 1};
         System.out.println("yoyo");
-        int[] abv = Sample.sortIntegers(arr);
-        assertEquals(ror,abv);
-    }
-    
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp() {
+        abv = Sample.sortIntegers(ar);
+        Assert.assertArrayEquals( ro, abv );
+        
         assertTrue(true);
     }
 }
