62be9cb2fe4ec4cb7fb8e49694f3cd8d73aedfff
Aniket Agarwal <agarwaan@tcd.ie>
2019-10-08 16:12:04 -0700

    Finishing Maven exercise

--- maven_exercise/exercise/src/test/java/java/SampleAppTest.java
+++ maven_exercise/exercise/src/test/java/java/SampleAppTest.java
@@ -1,8 +1,12 @@
 package com.exercise;
 
+import jdk.jfr.Timestamp;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import jdk.internal.jline.internal.TestAccessible;
 
 /**
  * Unit test for SampleApp.
@@ -23,6 +27,50 @@ public class SampleAppTest extends TestCase {
     public static Test suite() {
         return new TestSuite(SampleAppTest.class);
     }
+
+    @Test
+    void getInteger()
+    {
+        int arr[] = { 1, 2, 4, 5};
+        System.out.println("yoyo");
+        assertEquals(2,Sample.getInteger(arr, 2));
+    }
+
+    @Test
+    void getMin()
+    {
+        int arr[] = { 1, 2, 4, 5};
+        System.out.println("yoyo");
+        assertEquals(1,Sample.findMin(arr));
+    }
+
+    @Test
+    void getMax()
+    {
+        int arr[] = { 1, 2, 4, 5};
+        System.out.println("yoyo");
+        assertEquals(5,Sample.findMin(arr));
+    }
+
+    @Test
+    void getrev()
+    {
+        int arr[] = { 1, 2, 4, 5};
+        int ror[] = { 5, 4, 2, 1};
+        System.out.println("yoyo");
+        int[] abv = Sample.reverseArray(arr);
+        assertEquals(ror,abv);
+    }
+    @Test
+    void getsort()
+    {
+        int arr[] = { 1, 2, 4, 5};
+        int ror[] = { 5, 4, 2, 1};
+        System.out.println("yoyo");
+        int[] abv = Sample.sortIntegers(arr);
+        assertEquals(ror,abv);
+    }
+    
 
     /**
      * Rigourous Test :-)
