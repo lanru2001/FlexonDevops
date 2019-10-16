f3b61eb01f923572bbd141c5ee8b98fdac35fdb3
Aniket Agarwal <agarwaan@tcd.ie>
2019-10-16 11:15:55 -0700

    learning interfaces

---
+++ Practice Codes/interfaces.java
@@ -0,0 +1,13 @@
+
+
+public interface interfaces
+{
+    void changegear(int a);
+    void speedup(int a);
+    void applybrakes(int a);
+
+    static void displaymessage()
+    {
+        System.out.println("InterfaceMethod");
+    }
+}
